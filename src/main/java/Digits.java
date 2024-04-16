aa1789e37d71e70de7e5f2425e84aad82f4f1fb7
saaltamirano1 <143123836+saaltamirano1@users.noreply.github.com>
2024-04-15 21:47:56 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,14 +6,27 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
+	{ 
+		digitLIst = new ArrayList <Integer>;
+		if(num == 0){
+			digitList.add(0);
+		}
+		int n = num;
+		else if(num > 0 ){
+			digitList.add(0,n%10);
+			n = n%10;
+		}
 	    
 	}
 
 	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
-
+	{ 
+		for(int i = 1; i < didgitList.size(); i++){
+			if(digiitList.get(i, i+1) > digitList.get(i-1, i)){
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public String toString()
